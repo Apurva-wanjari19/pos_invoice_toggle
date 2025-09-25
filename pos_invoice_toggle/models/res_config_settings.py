b74cb33ee7f91e830df118f1763533d60bc0a14a
Apurva-wanjari19 <apurvawanjari175@gmail.com>
2025-09-25 11:19:17 +0530

    Delete pos_invoice_toggle directory

--- pos_invoice_toggle/models/res_config_settings.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    hide_pos_invoice_button = fields.Boolean(
-        related="pos_config_id.hide_pos_invoice_button",
-        readonly=False
-    )
